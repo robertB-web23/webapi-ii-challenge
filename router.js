45265bdc8f56faee90991e906a0754b26d032739
Robert Benavidez <murabitbenavidez@gmail.com>
2019-10-30 16:47:47 -0400

    all get routes

--- router.js
+++ router.js
@@ -18,6 +18,43 @@ router.get('/', (req, res) => {
         })
 })
 
+// GET post by ID
+
+router.get('/:id', (req,res) => {
+    const id = req.params.id;
+
+    Posts.findById(id)
+        .then(([post]) => {
+            
+            if(!post) {
+                res.status(404).json({message:'post id does not exist'})
+            } else {
+                res.status(200).json(post)
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
+
+// GET comments
+
+router.get('/:id/comments', (req,res) => {
+    const postId = req.params.id;
+
+    Posts.findPostComments(postId)
+        .then(comment => {
+            if(comment.length > 0) {
+                res.status(200).json(comment)
+            } else {
+                res.status(404).json({message: "The post with the specified ID does not exist"})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "The comments information could not be retrieved", error });
+            });
+})
+
 
 
 module.exports = router;
